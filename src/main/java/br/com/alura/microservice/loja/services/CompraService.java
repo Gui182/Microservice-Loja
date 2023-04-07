cbaacecaf0fd856e035ad68be5f297b5c9f1e389
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-04-06 23:59:33 -0300

    06/04 noite

--- src/main/java/br/com/alura/microservice/loja/services/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/services/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,13 @@ import br.com.alura.microservice.loja.dto.InforFornecedorDTO;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate client;
+
     public void realizaCompra(CompraDTO compra) {
 
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InforFornecedorDTO> exchange = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+        
+        ResponseEntity<InforFornecedorDTO> exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
         HttpMethod.GET, null, InforFornecedorDTO.class);
 
         System.out.println(exchange.getBody().getEndereco());
